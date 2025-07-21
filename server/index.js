f3db900ea01cf6056cf310fe119509d1dae73a29
Abhishek <abhisheksangule6@gmail.com>
2025-07-21 12:06:26 +0530

    remove the / from route

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: [
-    'https://sahayakai-mu.vercel.app/ ',
+    'https://sahayakai-mu.vercel.app ',
     'http://localhost:5173'
   ],
   credentials: true
